0c31137b41a4fe264d383f4a0d21a13b2aecaa74
Alexs <ale.osantos@hotmail.com>
2025-10-30 17:55:10 +0000

    feat: add  user authentication with login and logout routes

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
-from flask import Flask
+import bcrypt
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+from flask_login import login_required, login_user, logout_user, current_user
 from database import db
 from model.food import Food
 from model.user import User
+from flask_login import login_manager, LoginManager
 
 load_dotenv()
 
@@ -11,11 +14,36 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('MYSQL_DATABASE_URI')
 
-db.init_app(app)
+login_manager = LoginManager()
 
-@app.route('/')
-def home():
-    return "Welcome to the Daily Diet API!"
+db.init_app(app)
+login_manager.init_app(app)
+
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+        if user and password:
+            login_user(user)
+            return jsonify({'message': 'Logged in successfully'})
+    return jsonify({'message': 'Invalid credentials'}), 401
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Logged out successfully'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
